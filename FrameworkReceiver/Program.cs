187c1dac9113c7ec44330921f51d38144dffe9d6
krishnakanth Dasi <krishnakanthdasi@gmail.com>
2021-09-21 20:12:47 +0530

    Add project files.

---
+++ FrameworkReceiver/Program.cs
@@ -0,0 +1,23 @@
+﻿using NetMQ.Sockets;
+using NetMQ;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrameworkReceiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var socket = new ResponseSocket("@tcp://localhost:5500");
+
+            string message = socket.ReceiveFrameString();
+            Console.WriteLine("Message : " + message);
+
+            Console.ReadLine();
+        }
+    }
+}
